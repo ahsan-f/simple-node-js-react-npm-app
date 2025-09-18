96d21fb6ae160fbc61cb4b8eac5a52bf85703014
ahsan-f <140732909+ahsan-f@users.noreply.github.com>
2025-09-18 10:21:16 +0500

    Update kill.sh

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -4,4 +4,14 @@ echo 'The following command terminates the "npm start" process using its PID'
 echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
 echo 'was executed.'
 set -x
-kill $(cat .pidfile)
+
+if [ -f .pidfile ]; then
+  PID=$(cat .pidfile)
+  if kill -0 "$PID" 2>/dev/null; then
+    kill "$PID"
+  else
+    echo "Process $PID does not exist."
+  fi
+else
+  echo ".pidfile not found."
+fi
